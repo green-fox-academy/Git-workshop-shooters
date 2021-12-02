40c14f0aaf1144f564f2d6637ecbb797e8071228
PavelBla <pavel.blazekk@gmail.com>
2021-12-02 14:38:53 +0100

    init

---
+++ gittrayning/gittrayning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gittrayning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
