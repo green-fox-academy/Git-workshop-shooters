8c0eaa711b0a29e8259c2f201dc95916cce106c1
PavelBla <pavel.blazekk@gmail.com>
2021-12-02 14:48:25 +0100

    b1

---
+++ gittrayning/gittrayning/BClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittrayning
+{
+    class BClass
+    {
+        public void Count()
+        {
+            Console.WriteLine("My age is 32");
+        }
+    }
+}
