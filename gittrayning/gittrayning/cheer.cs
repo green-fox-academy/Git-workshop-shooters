ab40eb47d7fb14995395363ac01271a38cbb4511
Miroslav Jiranek <jiranekmiroslav@gmail.com>
2021-12-02 14:58:50 +0100

    c1

---
+++ gittrayning/gittrayning/cheer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittrayning
+{
+    class cheer
+    {
+        public string NameOfTheTeam()
+        {
+            return "C# shooters";
+        }
+    }
+}
