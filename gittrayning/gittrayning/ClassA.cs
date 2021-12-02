f4c42bb5d11215830b6d4849c9f245e0a5d2e6dd
AdamSattler <adamsat@seznam.cz>
2021-12-02 15:18:44 +0100

    a1

---
+++ gittrayning/gittrayning/ClassA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittrayning
+{
+    class ClassA
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Adam");
+        }
+    }
+}
