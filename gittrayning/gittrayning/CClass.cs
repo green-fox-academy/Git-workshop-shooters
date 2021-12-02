4bef71d7eba4db7f460e4fed0317efe2739ab483
Miroslav Jiranek <jiranekmiroslav@gmail.com>
2021-12-02 15:54:24 +0100

    c5

--- gittrayning/gittrayning/CClass.cs
+++ gittrayning/gittrayning/CClass.cs
@@ -8,9 +8,9 @@ namespace gittrayning
 {
     class Cclass
     {
-        public string Cheer(string firstParam)
+        public string Cheer(string firstParam, string secongParam)
         {
-            return $"C# shooters {firstParam}";
+            return $"C# shooters {firstParam}{secongParam}";
         }
     }
 }
